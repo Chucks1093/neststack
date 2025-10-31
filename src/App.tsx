da6f52cc94ff85c19c6d984a9edd0e26325b1e7c
Anioke Sebastian <aniokechukwudi7@gmail.com>
2025-10-31 19:37:35 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import './app.css';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
 const router = createBrowserRouter([
-   {
-      path: '/',
-      element: <div>My App</div>,
-   },
+	{
+		path: '/',
+		element: (
+			<div className="text-3xl font-bold text-center mt-40">
+				Work in Progress...
+			</div>
+		),
+	},
 ]);
 
 function App() {
-   return (
-      <>
-         <RouterProvider router={router} />
-      </>
-   );
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
